f9ca19890eba08d874310e2f3f54802d512b05a3
Maurine Duet <maurineduet@gmail.com>
2023-02-01 21:35:47 +0100

    Ajustements

--- js/main.js
+++ js/main.js
@@ -5,31 +5,32 @@ hover.src = "../assets/sounds/hover2.mp3"
 
 const playBtn = document.querySelector("[data-play-btn]")
 
-if(playBtn) {
+if (playBtn) {
 
-playBtn.addEventListener("mouseover", (play)=> {
-    hover.play()
-})
+    playBtn.addEventListener("mouseover", (play) => {
+        hover.play()
+    })
 
-playBtn.addEventListener("mouseleave", (play)=> {
-    hover.pause()
-})
+    playBtn.addEventListener("mouseleave", (play) => {
+        hover.pause()
+    })
 
 }
 
 // Audio button play on click //
 
-if(playBtn) {
+if (playBtn) {
 
-const playStart = new Audio()
-playStart.src = "../assets/sounds/play.mp3"
+    const playStart = new Audio()
+    playStart.src = "../assets/sounds/play.mp3"
 
-playBtn.addEventListener("click", (play) => {
-    playStart.play()
+    playBtn.addEventListener("click", (play) => {
+        playStart.play()
 
-    setTimeout(() => {
-    document.location = 'form.html' }, 1500)
-})
+        setTimeout(() => {
+            document.location = 'form.html'
+        }, 1500)
+    })
 
 }
 
@@ -42,13 +43,13 @@ hoverFooter.src = "../assets/sounds/hover3.mp3"
 const footerBtn = document.querySelectorAll("[data-play-footer]")
 
 footerBtn.forEach(element => {
-    
-element.addEventListener("mouseover", (play)=> {
-    hoverFooter.play()
-})
 
-element.addEventListener("mouseleave", (play)=> {
-    hoverFooter.pause()
-})
+    element.addEventListener("mouseover", (play) => {
+        hoverFooter.play()
+    })
 
-});
+    element.addEventListener("mouseleave", (play) => {
+        hoverFooter.pause()
+    })
+
+})
